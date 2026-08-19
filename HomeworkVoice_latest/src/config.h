d61b936b676f1e88faf3bc191835b2fd73c4fcb2
ゆう 原 優 <yh192168@gmail.com>
2026-08-19 19:40:58 +0900

    Update WiFi credentials in config.h

--- HomeworkVoice_latest/src/config.h
+++ HomeworkVoice_latest/src/config.h
@@ -1,8 +1,8 @@
 #pragma once
 #include <Arduino.h>
 namespace config {
-inline constexpr char kWifiSsid[]="52693532-2.4G";
-inline constexpr char kWifiPassword[]="43954892";
+inline constexpr char kWifiSsid[]="SSID1234";
+inline constexpr char kWifiPassword[]="Password1234";
 inline constexpr char kApiBaseUrl[]="http://10.32.1.60:8001";
 inline constexpr uint32_t kHttpTimeoutMs=30000;
 inline constexpr uint8_t kHttpRetries=3;
